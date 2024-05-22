4363456f53bdc27bdc6e1799cb0ca50b79607aea
frojak4 <Frojak4@gmail.com>
2024-05-22 14:02:19 +0200

    Initial commit

---
+++ Bossfight/GameCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight
+{
+    internal class GameCharacter
+    {
+        public string Name;
+        public int HP;
+        public int Strength;
+        public int Stamina;
+        public int MaxStamina;
+
+        public GameCharacter(string name, int hp, int strength, int stamina, int maxstamina)
+        {
+            Name = name;
+            HP = hp;
+            Strength = strength;
+            Stamina = stamina;
+            MaxStamina = maxstamina;
+        }
+
+        public static void Fight(GameCharacter attacker, GameCharacter defender)
+        {
+            defender.HP -= attacker.Strength;
+            attacker.Stamina -= 10;
+            Console.WriteLine($"{attacker.Name} attacked {defender.Name} for {attacker.Strength}, {defender.Name} now has {defender.HP} HP");
+            Console.ReadKey(true);
+        }
+
+        public static void Recharge(GameCharacter user)
+        {
+            user.Stamina = user.MaxStamina;
+            Console.WriteLine($"{user.Name}'s stamina is now {user.Stamina}");
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ Bossfight/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+
+namespace Bossfight
+{
+    internal class Program
+    {
+        
+        public static GameCharacter Player = new GameCharacter("Frode", 100, 20, 40, 40);
+        public static GameCharacter Boss = new GameCharacter("The Duke of Flies", 400, 20, 10, 40);
+        public static bool gameState = true;
+        static void Main(string[] args)
+        {
+            while (Player.HP > 0 && Boss.HP > 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"{Player.Name}     vs     {Boss.Name}");
+                Console.WriteLine($"Health: {Player.HP}          Health:{Boss.HP}");
+                Console.WriteLine($"Strength: {Player.Strength}          Strength:{Boss.Strength}");
+                Console.WriteLine($"Stamina: {Player.Stamina}          Stamina:{Boss.Stamina}");
+                Console.WriteLine();
+                Console.WriteLine("1. Attack");
+                Console.WriteLine("2. Recharge");
+
+                string userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    GameCharacter.Fight(Player, Boss);
+                } else if (userInput == "2")
+                {
+                    GameCharacter.Recharge(Player);
+                }
+                Random random = new Random();
+                int cpuInput = random.Next(1, 3);
+                
+                    if (cpuInput == 1)
+                    {
+                        GameCharacter.Fight(Boss, Player);
+                    } else if (cpuInput == 2)
+                    {
+                        GameCharacter.Recharge(Boss);
+                    }
+                
+            }
+        }
+    }
+}
